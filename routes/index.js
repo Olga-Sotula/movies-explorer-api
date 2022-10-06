ed9d4a9eb3fb7cbd91f9a004e17a9131eb748f03
Olga Sotula <olga.sotula@itctg.ru>
2022-10-06 16:48:06 +0300

    2.4 add auth

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { auth } = require('../middlewares/auth');
 const { authRouter } = require('./auth');
 const { userRouter } = require('./users');
 const { movieRouter } = require('./movies');
 
 router.use(authRouter);
-//app.use(auth);
+router.use(auth);
 router.use('/users', userRouter);
 router.use('/movies', movieRouter);
 
