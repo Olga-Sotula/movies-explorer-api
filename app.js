52738a79c87fa014d8bff399eb19bb2457e13e9e
Olga Sotula <olga.sotula@itctg.ru>
2022-10-06 16:54:02 +0300

    2.5 add error logger

--- app.js
+++ app.js
@@ -4,22 +4,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 //const { errors } = require('celebrate');
-//const cors = require('cors');
+const cors = require('cors');
 
 const router = require('./routes/index');
-//const { auth } = require('./middlewares/auth');
-//const { userRouter } = require('./routes/users');
-/*const { cardRouter } = require('./routes/cards');
-const { authRouter } = require('./routes/auth');
-const { errorHandler } = require('./errors/errorHandler');
-const { ErrorNotFound } = require('./errors/ErrorNotFound');
-const { requestLogger, errorLogger } = require('./middlewares/logger');*/
+//const { errorHandler } = require('./errors/errorHandler');
+//const { ErrorNotFound } = require('./errors/ErrorNotFound');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
@@ -27,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017/moviesdb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(requestLogger); // подключаем логгер запросов
+app.use(requestLogger); // подключаем логгер запросов
 
 app.get('/crash-test', () => { // crash-test
   setTimeout(() => {
